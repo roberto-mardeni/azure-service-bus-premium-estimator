0389f8c1ef1a711c4dd57e874c89259219b4d0b0
roberto-mardeni <rmardeni@gmail.com>
2022-03-14 13:49:22 -0400

    Initial commit

---
+++ src/AzureServiceBusEstimator/postMessage.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace My.AzureServiceBusEstimator
+{
+    public static class postMessage
+    {
+        [FunctionName("postMessage")]
+        [return: ServiceBus("myqueue", Connection = "ServiceBusConnection")]
+        public static string Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "post/{sizeInMb:int?}")] HttpRequest req,
+            ILogger log, ExecutionContext ctx, int? sizeInMb)
+        {
+            var filePath = Path.Combine(ctx.FunctionAppDirectory, "payloads", $"{sizeInMb}mb.txt");
+            if (System.IO.File.Exists(filePath))
+            {
+                log.LogInformation($"Posting message of {sizeInMb} MB, using {filePath}");
+                return System.IO.File.ReadAllText(filePath);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid sizeInMb provided, only 1,2,5 or 10 supported!");
+            }
+        }
+    }
+}
